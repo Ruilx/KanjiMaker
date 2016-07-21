c329a7ebf6d03a3221af939154be004c362f321c
Ruilx <799382072@qq.com>
2016-07-21 12:49:56 +0800

    Translate build up, auto inscript Chinese(zh_CN.UTF-8) language to program. Ruilx

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	QString translatorFileName = "zh_CN.UTF-8.qm";
+	QTranslator *trans = new QTranslator(&a);
+	if(trans->load(translatorFileName)){
+		a.installTranslator(trans);
+	}
+
 	MainW w;
 	w.show();
 
